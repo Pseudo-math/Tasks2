300f0ccc00f5c3a1fdcdb7818add6658f46d7704
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-11-29 18:39:35 +0300

    Create NinPowerM.cs

---
+++ NinPowerM.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace AlgorithmsDataStructures {
+  	
+	public class Recursion {
+        
+        private static double RecursionPower(double n, int m) {
+			if (m == 0) return 1;
+			if (m % 2 == 0) return RecursionPower(n * n, m / 2);
+			return n * RecursionPower(n, m - 1);
+		}
+		
+		public static double NInPowerM(double n, int m) {
+			if (n < 0) throw new ArgumentException("First argument must be a nonnegative number.");
+			if (n == 0 && m <= 0) throw new ArgumentException("Zero cannot raise to a nonpositive power.");
+			if (n == 0) return 0;
+			if (n == 1 || m == 0) return 1;
+			if (m == 1) return n;
+			if (m > 0) return RecursionPower(n, m);
+			else return RecursionPower(1 / n, -m); 
+		}
+	}
+}
