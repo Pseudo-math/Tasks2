1394552e613e5924ad3434bbba711ed1c1e6a6fc
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-12-02 14:33:52 +0300

    Update NInPowerMEliminated.cs

--- NInPowerMEliminated.cs
+++ NInPowerMEliminated.cs
@@ -11,22 +11,22 @@ namespace AlgorithmsDataStructures {
 			if (n == 0 && m <= 0) throw new ArgumentException("Zero cannot raise to a nonpositve power.");
 			if (n == 0) return 0;
 			if (n == 1) return 1;
-      int acc = 1;
-      while (m != 0) {
-        if (m < 0) {
-          n = 1 / n;
-          m = -m;
-        }
-        else if (m % 2 == 0) {
-          n *= n;
-          m /= 2;
-        }
-        else {
-          m--;
-          acc *= n;
-        }
-      }
-      return acc;
+			double acc = 1;
+		        while (m != 0) {
+				if (m < 0) {
+					n = 1 / n;
+					m = -m;
+				}
+				else if (m % 2 == 0) {
+					n *= n;
+					m /= 2;
+				}
+				else {
+					m--;
+					acc *= n;
+				}
+			}
+			return acc;
 		}
 	}    
 }
