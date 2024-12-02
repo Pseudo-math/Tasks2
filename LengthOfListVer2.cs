f8a9340ed42aadf765f44d89f2480958004ddea3
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-12-02 16:35:26 +0300

    Create LengthOfListVer2.cs

---
+++ LengthOfListVer2.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures {
+
+	public class Recursion {
+
+		public static int LengthOfList<T>(LinkedList<T> list) {
+			var newList = new LinkedList<T>(list); // Не изменяем исходный список
+			return LengthOfListHelp(newList, 0);
+		}
+    private int LengthOfListHelp<T>(LinkedList<T> newList, int accLength) {
+			if (newList.Count == 0) return accLength;
+			newList.RemoveFirst();
+			return LengthOfList(newList, accLength + 1);
+		}
+	}
+}
