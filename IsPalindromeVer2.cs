46e5426f3be5b2b72c931b8e7733ed60ea48ecc4
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-12-02 16:40:33 +0300

    Update IsPalindromeVer2.cs

--- IsPalindromeVer2.cs
+++ IsPalindromeVer2.cs
@@ -8,10 +8,10 @@ namespace AlgorithmsDataStructures {
 			if (string.IsNullOrEmpty(maybePalindrome)) return true;
 			return IsPalindrome(maybePalindrome, 0);
 		}
-    private bool IsPalindromeHelp(string maybePalindrome, int step) {
-      if (maybePalindrome.Length - step <= 1) return true;
-      if (maybePalindrome[step] != maybePalindrome[maybePalindrome.Length - 1 - step]) return false;
-      return IsPalindrome(maybePalindrome, step + 1);
-    }
+    		private bool IsPalindromeHelp(string maybePalindrome, int step) {
+      			if (maybePalindrome.Length - step <= 1) return true;
+      			if (maybePalindrome[step] != maybePalindrome[maybePalindrome.Length - 1 - step]) return false;
+      			return IsPalindrome(maybePalindrome, step + 1);
+    		}
 	}
 }
