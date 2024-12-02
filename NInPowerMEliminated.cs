b44960eb252e01a86b66005df628bf1dfe6c1675
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-12-02 17:02:12 +0300

    Update NInPowerMEliminated.cs

--- NInPowerMEliminated.cs
+++ NInPowerMEliminated.cs
@@ -6,16 +6,16 @@ namespace AlgorithmsDataStructures {
   	
 	public class Recursion {
 
-		public static double NInPowerM(double n, int m, double acc = 1) {
+		public static double NInPowerM(double n, int m) {
 			if (n < 0) throw new ArgumentException("First argument must be a nonnegative number.");
 			if (n == 0 && m <= 0) throw new ArgumentException("Zero cannot raise to a nonpositve power.");
 			if (n == 0) return 0;
 			if (n == 1) return 1;
+			if (m < 0) return NInPowerMHelp(1 / n, -m, 1);
+		    	return NInPowerMHelp(n, m, 1);
+		}
+		public static double NInPowerMHelp(double n, int m, double acc) {
 			if (m == 0) return acc;
-			if (m < 0) {
-				n = 1 / n;
-				m = -m;
-			}
 			if (m % 2 == 0) {
 				n = n * n;
 				m = m / 2;
@@ -24,7 +24,8 @@ namespace AlgorithmsDataStructures {
 				m--;
 				acc = acc * n;
 			}
-		    	return NInPowerM(n, m, acc);
+		    	return NInPowerMHelp(n, m, acc);
 		}
+		
 	}  
 }
