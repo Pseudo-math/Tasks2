645f3f436a71ea237f0d76a2960e0bf8e9e3d128
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-12-02 16:38:20 +0300

    Create IsPalindromeVer2.cs

---
+++ IsPalindromeVer2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace AlgorithmsDataStructures {
+
+	public class Recursion {
+
+		public static bool IsPalindrome(string maybePalindrome) {
+			if (string.IsNullOrEmpty(maybePalindrome)) return true;
+			return IsPalindrome(maybePalindrome, 0);
+		}
+    private bool IsPalindromeHelp(string maybePalindrome, int step) {
+      if (maybePalindrome.Length - step <= 1) return true;
+      if (maybePalindrome[step] != maybePalindrome[maybePalindrome.Length - 1 - step]) return false;
+      return IsPalindrome(maybePalindrome, step + 1);
+    }
+	}
+}
