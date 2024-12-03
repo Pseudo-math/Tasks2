ea405e3e4e74be2861081fdfe9de422cbb543b2a
Pseudo-math <118271466+Pseudo-math@users.noreply.github.com>
2024-12-03 18:33:38 +0300

    Create evenValuesVer1.cs

---
+++ evenValuesVer1.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures {
+
+	public class Recursion {
+
+		public void evenValues<T>(List<T> list) {
+			evenValuesHelp(List, 0);
+		}
+   		private void evenValuesHelp<T>(LinkedList<T> list, int index) {
+			if (index == list.Count) return;
+	    if (list[index] % 2) Console.WriteLine(list[index]);
+			evenValuesHelp(list, index + 1);
+		}
+	}
+}
